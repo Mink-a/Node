37589c4120b9de4aa51e09bb297e008e80077029
Mink-a <rahulakhant@gmail.com>
2022-08-16 14:08:28 +0630

    Start with Express JS

---
+++ main.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const EventEmitter = require("events");
+const myEvent = new EventEmitter();
+
+const app = express();
+const PORT = 5000;
+
+myEvent.on("test-event", (data) => {
+  console.log("test-event is listening");
+  console.log(data);
+});
+
+app.get("/", (req, res) => {
+  myEvent.emit("test-event", {name: 'MInk'});
+  res.send("helo");
+});
+
+app.listen(PORT, () => {
+  console.log(`app is running at http://localhost:${PORT}`);
+});
