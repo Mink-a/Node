7f6e1e72e8716a4270a1124570fb2e32c0a01a9c
Mink-a <rahulakhant@gmail.com>
2022-08-16 12:02:52 +0630

    Add some pages with HTML

--- app.js
+++ app.js
@@ -1,9 +1,32 @@
-const http = require('http');
+const http = require("http");
+const fs = require("fs");
+const PORT = 4000;
 
 // Create a local server to receive data from
 const server = http.createServer((req, res) => {
-  res.writeHead(200, {'Content-Type':'application/json'})
-  res.end(JSON.stringify({data: "helo node"}))
+  if (req.url === "/") {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    fs.readFile("pages/index.html", "utf-8", (err, data) => {
+      if (err) throw err;
+      res.write(data);
+      res.end();
+    });
+  } else if (req.url === "/about") {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    fs.readFile("pages/about.html", "utf-8", (err, data) => {
+      if (err) throw err;
+      res.write(data);
+      res.end();
+    });
+  } else {
+    res.writeHead(404, { "Content-Type": "text/html" });
+    fs.readFile("pages/404.html", "utf-8", (err, data) => {
+      if (err) throw err;
+      res.write(data);
+      res.end();
+    });
+  }
 });
 
-server.listen(8000);
+console.log(`server is running at http://localhost:${PORT}`);
+server.listen(PORT);
