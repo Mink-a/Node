175b241c3220366d31d881a80b4dfa3526b92dd9
Mink-a <rahulakhant@gmail.com>
2022-08-17 11:21:54 +0630

    test MongoDb

--- routes/book.js
+++ routes/book.js
@@ -1,17 +1,33 @@
 const express = require("express");
 const bookRouter = express.Router();
+const connect = require('./../database/db')
 
 bookRouter
   .route("/")
-  .get((req, res) => {
+  .get(async (req, res) => {
+    // const db = await connect()
     res.send("All books");
   })
-  .post((req, res) => {
-    res.json({ title: "Sherlock Holmes" });
+  .post(async (req, res) => {
+    const db = await connect()
+    const  data = {
+      title: "harry potter",
+      author: 'JK'
+    }
+    await db.collection('book').insertOne(data)
+    res.json({ data: "Book Stored" });
   });
 
-bookRouter.get("/:id", (req, res) => {
-  res.send(`single book id:${req.params.id}`);
-});
+bookRouter
+  .route("./:id")
+  .get((req, res) => {
+    res.send(`single book id:${req.params.id} to get Details`);
+  })
+  .patch((req, res) => {
+    res.send(`single book id:${req.params.id} to update`);
+  })
+  .delete((req, res) => {
+    res.send(`single book id:${req.params.id} to delete`);
+  });
 
 module.exports = bookRouter;
